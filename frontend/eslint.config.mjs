72156d67de290459057f747c1145ca5cbf8abf0f
Vindiya Wickramathunga <vindiyawickramathunga@gmail.com>
2025-03-17 14:50:39 +0530

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@next/next/no-img-element": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
