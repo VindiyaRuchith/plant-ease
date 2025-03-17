95637a34a19103e03962d59d7a1ce00a19a36df1
Vindiya Wickramathunga <vindiyawickramathunga@gmail.com>
2025-03-17 20:19:40 +0530

    Added backend and frontend

--- backend/app.py
+++ backend/app.py
@@ -53,4 +53,5 @@ def classify_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Google Cloud assigns a port
+    app.run(host='0.0.0.0', port=port, debug=True)
