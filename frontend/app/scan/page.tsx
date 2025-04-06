6b1774c724dfc0bdf534d7927a4a1f75418c9eec
Vindiya Wickramathunga <85401783+VindiyaRuchith@users.noreply.github.com>
2025-04-06 13:16:42 +0530

    Update page.tsx

--- frontend/app/scan/page.tsx
+++ frontend/app/scan/page.tsx
@@ -36,10 +36,9 @@ export default function ScanPage() {
     setIsLoading(true);
     try {
       const response = await fetch(
-        `https://performance-556001537402.asia-south1.run.app//classify?cam_type=${camType}`,
+        `https://grad-556001537402.asia-south1.run.app/classify?cam_type=${camType}`,
         {
           method: "POST",
-          
           body: formData,
         }
       );
